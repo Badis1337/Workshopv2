ef30b31532297638f462360802b0b5a03b51301f
Maalej Mohamed Badis <35582138+Badis1337@users.noreply.github.com>
2019-04-11 00:53:58 +0100

    workshop complet .

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ mongo.on('open',()=>{
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var optionsRouter =require('./routes/options');
 
 var app = express();
 
@@ -37,7 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/options',optionsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
